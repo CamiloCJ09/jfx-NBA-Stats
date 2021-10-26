9351901cf3997ae2c9fc01fba0ea2e9c1f2825d8
Johan794 <johan.ricardo1104@gmail.com>
2021-10-25 21:00:01 -0500

    Creating test module

---
+++ test/model/ownImplementation/classes/AVLTreeTest.java
@@ -0,0 +1,63 @@
+package model.ownImplementation.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AVLTreeTest {
+
+
+
+    @Test
+    void testIsBalanced() {
+    }
+
+    @Test
+    void balance() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+
+
+    @Test
+    void testSearch() {
+    }
+
+    @Test
+    void testSuccessor() {
+    }
+
+
+
+    @Test
+    void testMin() {
+    }
+
+    @Test
+    void testMax() {
+    }
+
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getRollingFactor() {
+    }
+
+    @Test
+    void rigthRotate() {
+    }
+
+    @Test
+    void leftRotate() {
+    }
+}
---
+++ test/model/ownImplementation/classes/BinarySearchTreeTest.java
@@ -0,0 +1,65 @@
+package model.ownImplementation.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+    private BinarySearchTree<Integer> tree;
+
+    public void setup1() {
+        tree = new BinarySearchTree<>();
+    }
+
+    public void setup2() {
+        tree = new BinarySearchTree<>();
+        tree.addNode(3);
+        tree.addNode(5);
+    }
+
+    public void setup3() {
+        tree = new BinarySearchTree<>();
+        tree.addNode(3);
+        tree.addNode(5);
+        tree.addNode(1);
+        tree.addNode(2);
+        tree.addNode(7);
+        tree.addNode(8);
+    }
+    @Test
+    public void addNode1(){
+        setup1();
+        tree.addNode(3);
+        tree.addNode(5);
+        assertFalse(tree.isEmpty());
+    }
+    @Test
+    public void delete(){
+        setup2();
+        tree.delete(5);
+        System.out.println("Luego de borrar "+tree.printInOrder());
+        assertEquals(1,tree.getWeight());
+    }
+
+    @Test
+    public void search(){
+        setup2();
+        assertTrue(tree.search(5)!=null);
+    }
+
+    @Test
+    public void successor1(){
+        setup3();
+        assertEquals(3, tree.successor(tree.search(2)).getValue());
+    }
+    @Test
+    public void successor2(){
+        setup3();
+        assertEquals(7, tree.successor(tree.search(5)).getValue());
+    }
+    @Test
+    public void successor3(){
+        setup3();
+        assertEquals(5, tree.successor(tree.search(3)).getValue());
+    }
+}
